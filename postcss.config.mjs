e8b82e1f4635536c0c3cee4349667babbb240de7
Your Name <your.email@example.com>
2025-03-24 15:00:06 +0100

    Fix Tailwind CSS configuration and dependencies

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-const config = {
+export default {
   plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
+    'tailwindcss': {},
+    'autoprefixer': {},
+  }
 };
-
-export default config;
